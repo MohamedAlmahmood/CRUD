4e615b66084f8ae609f5fba92d7b5f0b9a17688a
MohamedAlmahmood <xhamadeex@gmail.com>
2021-01-22 22:15:30 +0300

    created the server side using nodejs

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.send("devStart wokring"); //send the response to the front end
+});
+
+/*
+When we are at the home url "http://localhost:3001/" we want the page to res 'respond' with hello. 
+req: require: get information from the front end.
+res: response: send information to the front end. 
+*/
+
+
+
+app.listen(3001, ()=>{
+    console.log("runnning on port 3001 using devStart")
+});
+
+/*
+1. go to the folder
+2. type 'npm init'
+3. click enter to all the questions.
+4. package.json is created.
+5. create a file called index.js
+6. type 'npm isntall express body-parser mysql'
+7. type 'npm install nodemon' 
+8. add to package.json below the scripts section:
+    a. "start": "node index.js",
+    b. "devStart": "nodemon index.js",
+9. now to run the server type 'npm run devStart'
+*/
